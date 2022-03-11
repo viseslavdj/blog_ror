4571e381357a0ffd8d2bd0c26b94fb113e9d057d
Viseslav <vios.djuric212@gmail.com>
2022-03-11 14:10:15 +0100

    First part done, it works
    Need to update project look (frontend)

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   root "articles#index"
-
-  get "/articles", to: "articles#index"
-
+  
+  resources :articles do
+    resources :comments
+  end
+  
 end
